849190a959b40d5b118710a99584f376803283c4
cuizhenlong <czl389@163.com>
2025-03-18 14:05:16 +0800

    Update

--- 42.trapping-rain-water.py
+++ 42.trapping-rain-water.py
@@ -17,23 +17,39 @@ from typing import *
 # @lc code=start
 class Solution:
     def trap(self, height: List[int]) -> int:
-        prefix = [0] *len(height)
-        suffix = [0] *len(height)
-        for i in range(len(height)):
-            if i == 0:
-                prefix[i] = height[i]
-            else:
-                prefix[i] = max(prefix[i-1], height[i])
+        # prefix = [0] *len(height)
+        # suffix = [0] *len(height)
+        # for i in range(len(height)):
+        #     if i == 0:
+        #         prefix[i] = height[i]
+        #     else:
+        #         prefix[i] = max(prefix[i-1], height[i])
         
-        for i in range(len(height)-1, -1, -1):
-            if i == len(height)-1:
-                suffix[i] = height[i]
-            else:
-                suffix[i] = max(suffix[i+1], height[i])
+        # for i in range(len(height)-1, -1, -1):
+        #     if i == len(height)-1:
+        #         suffix[i] = height[i]
+        #     else:
+        #         suffix[i] = max(suffix[i+1], height[i])
+        
+        # result = 0
+        # for i in range(len(height)):
+        #     result = result + min(prefix[i], suffix[i]) - height[i]
+        # return result
+        left = 0
+        right = len(height)-1
+        prefix_max = 0
+        suffix_max = 0
         
         result = 0
-        for i in range(len(height)):
-            result = result + min(prefix[i], suffix[i]) - height[i]
+        while left <= right:
+            prefix_max = max(prefix_max, height[left])
+            suffix_max = max(suffix_max, height[right])
+            if prefix_max <= suffix_max:
+                result = result + prefix_max - height[left]
+                left = left+1
+            else:
+                result = result + suffix_max - height[right]
+                right = right-1
         return result
 
 
